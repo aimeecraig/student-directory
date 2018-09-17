4a837901c2ef9ec2481c641c85c077085a08de1a
Aimée Craig <aimeecraig1@gmail.com>
2018-09-17 10:01:20 +0100

    amended the input_students method to ask for a cohort and return september if the cohort is not valid

--- directory.rb
+++ directory.rb
@@ -1,21 +1,29 @@
 #!/usr/bin/env ruby
 
 def input_students
-  puts "Please enter the name of the student and their birth country, separated with a comma"
+  # define a list of valid cohort months
+  months = [ "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" ]
+  # prompt for user input
+  puts "Please enter the name of the student"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  user_input = gets.chomp
-  student = user_input.split(",")
+  name = gets.chomp
   # while the name is not empty, repeat this code
-  while !user_input.empty? do
-    # add the student hash to the array
-    students << {name: student[0], cohort: :november, birth_country: student[1]}
+  while !name.empty? do
+    puts "Please enter the cohort"
+    cohort = gets.chomp
+    # check for empty value
+    if cohort == nil || months.include?(cohort) == false
+      students << {name: name, cohort: :september, birth_country: "UK"}
+    else
+      students << {name: name, cohort: cohort.to_sym, birth_country: "UK"}
+    end
     puts "Now we have #{students.count} students"
     # get another name from the user
     user_input = gets.chomp
-    student = user_input.split(",")
+    name = user_input.split(",")
   end
   # return the array of students
   students
