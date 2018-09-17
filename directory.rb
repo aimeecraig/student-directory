98f0db8e56ea1aa7954a2fa49b423280a3772122
Aimée Craig <aimeecraig1@gmail.com>
2018-09-17 13:09:27 +0100

    save_students method created to save student list to csv

--- directory.rb
+++ directory.rb
@@ -13,6 +13,7 @@ def print_menu
   # present options to user
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -29,6 +30,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -119,6 +122,18 @@ def print_footer(names)
   elsif names.count > 1
     puts "Overall we have #{names.count} great students"
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:birth_country]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
